d220c778742c1c7bab87728b0ead63c869c52b5e
Aygun Yildiz <aygun.yildiz@oohmedia.com.au>
2021-05-07 16:29:59 +1000

    Updating files

--- cypress/integration/vamp_automated_test.js
+++ cypress/integration/vamp_automated_test.js
@@ -6,13 +6,14 @@ context('Vamp Automated Test Challenge', () => {
   })
 
   it('Vamp campaign quote page loads successfully', () => {
+    // This is a basic test to check the Vamp campaign quote staging page loads correctly
     cy.get('[data-cy="campaign-header"]')
       .contains('Welcome to Quote')
       .should('be.visible');
   })
 
   it('Test case 1 - Setting currency, budget, location & social channel will show performance metric results for an "Influencer" campaign type', () => {
-
+    // This test validates that performance metrics will return results when all necessary fields are set
     campaignQuotePage.selectCampaignType('Influencer');
     campaignQuotePage.setBudgetCurrencyToAUD();
     campaignQuotePage.setBudgetValue('2000');
@@ -24,7 +25,7 @@ context('Vamp Automated Test Challenge', () => {
   })
 
   it('Test case 2 - User is not able to save an "Influencer" campaign quote until all required fields are populated', () => {
-      
+    // This test checks that a user can save a campaign quote only when the required fields are entered
     campaignQuotePage.selectCampaignType('Influencer');
     campaignQuotePage.saveQuote();
     campaignQuotePage.assertRequiredFields();
@@ -40,16 +41,16 @@ context('Vamp Automated Test Challenge', () => {
   })
 
   it('Test case 3 - Validate fields that are not visible when the "Content" campaign type is selected', () => {
-
+    // This test validates that creator manangement, event attendance and ojbectives/socials channels do not appear for a Content campaign 
     campaignQuotePage.selectCampaignType('Content');
-    //campaignQuotePage.assertCreatorManagementNotVisible();
+    campaignQuotePage.assertCreatorManagementNotVisible();
     campaignQuotePage.assertEventAttendanceNotVisible();
     campaignQuotePage.assertObjectivesAreNotVisible();
     campaignQuotePage.assertSocialChannelFieldsNotVisible();
   })
 
   it('Test case 4 - Validate social channel bars are not visible and performance metrics show images and videos for a "Content" campaign type', () => {
-
+    // This test validates that social channel bars do not appear and performance metrics show just images and vides for a Content campaign
     campaignQuotePage.selectCampaignType('Content');
     campaignQuotePage.assertSocialChannelBarsNotVisibleUnderContent();
     campaignQuotePage.assertSocialChannelBarsNotVisibleInHeader();
